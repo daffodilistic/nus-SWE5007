5085ce2b1334ef30b587dec53a285e90c46e011b
desmond <sohskd@hotmail.com>
2023-06-06 20:57:59 +0800

    multi project

---
+++ rigsg/user-profile-management/src/main/java/com/nus/project/capstone/rigsg/UserProfileManagementApplication.java
@@ -0,0 +1,12 @@
+package com.nus.project.capstone.rigsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProfileManagementApplication.class, args);
+    }
+}
