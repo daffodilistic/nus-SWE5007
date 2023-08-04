efcbdaa8511129db2668456ddd73a10efbc6a7f3
Soh Thiam Hing <daffodilistic@gmail.com>
2023-08-04 16:56:22 +0800

    [devops] working command line script to build and deploy to app engine

--- deploy-appengine.sh
+++ deploy-appengine.sh
@@ -3,4 +3,4 @@ cd rigsg
 ./mvnw install
 # NOTE Might not work in Linux due the space between "-D" and "app.deploy.projectId"
 # ./mvnw --projects ${{ matrix.services }} --also-make clean package spring-boot:repackage -DskipTests=true
-./mvnw  --projects info-collection-system clean package appengine:deploy -D app.deploy.projectId=sapient-symbol-388402 -D app.deploy.version=test-v1
+./mvnw  --projects demo clean package spring-boot:repackage appengine:deploy -D app.deploy.projectId=sapient-symbol-388402 -D app.deploy.version=test-v1
