c563000e9a41a2846657f34249d856a69c4cda0c
PngKianMing <km.png.1988@gmail.com>
2023-08-12 22:22:58 +0800

    Add files via upload

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,2 +1,15 @@
-export const IDC_TEAM_BASE_URL = 'http://localhost:8082/';
-export const USER_INFO_BASE_URL = 'http://localhost:8081/';
+export const GET_ALL_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/users'
+export const USER_INFO_BASE_URL ='http://localhost:8081/userinfo/user'
+
+export const GET_ALL_IDC_TEAM_BASE_URL ='http://localhost:8082/idcteam/teams'
+export const IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/team'
+
+export const GET_ALL_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/metrics'
+export const IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/metric'
+
+export const GET_ALL_GAME_METRIC_BASE_URL = 'http://localhost:8083/games/metrics'
+export const GAME_METRIC_BASE_URL = 'http://localhost:8083/games/metric'
+
+export const GET_ALL_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/teams'
+export const GAME_TEAM_BASE_URL = 'http://localhost:8083/games/team'
+
