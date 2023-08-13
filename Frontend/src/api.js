698272188dee04182209e3fb27d1c01e8fe25fea
PngKianMing <km.png.1988@gmail.com>
2023-08-13 11:55:55 +0800

    Update API path

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,15 +1,31 @@
-export const GET_ALL_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/users'
-export const USER_INFO_BASE_URL ='http://localhost:8081/userinfo/user'
+//CONTESTANTS
+export const GET_ALL_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/view-all-users'
+export const UPDATE_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/update-user'
+export const CREATE_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/create-user'
 
-export const GET_ALL_IDC_TEAM_BASE_URL ='http://localhost:8082/idcteam/teams'
-export const IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/team'
+//IDC TEAM
+export const GET_ALL_IDC_TEAM_BASE_URL ='http://localhost:8082/idcteam/view-all-teams'
+export const VIEW_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/view-team'
+export const UPDATE_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/update-team'
+export const ADD_MEMBER_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/assign-user'
+export const CREATE_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/create-team'
+export const QUALIFY_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/qualify-team'
 
-export const GET_ALL_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/metrics'
-export const IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/metric'
+//GAME TEAM
+export const GET_ALL_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/view-all-teams'
+export const VIEW_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/view-team'
+export const UPDATE_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/update-team'
+export const ADD_MEMBER_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/assign-user'
+export const CREATE_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/create-team'
+export const QUALIFY_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/qualify-team'
 
-export const GET_ALL_GAME_METRIC_BASE_URL = 'http://localhost:8083/games/metrics'
-export const GAME_METRIC_BASE_URL = 'http://localhost:8083/games/metric'
+//IDC METRICS
+export const GET_ALL_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/view-all-metrics'
+export const CREATE_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/create-metric'
+export const UPDATE_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/update-metric'
 
-export const GET_ALL_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/teams'
-export const GAME_TEAM_BASE_URL = 'http://localhost:8083/games/team'
+//GAME METRICS
+export const GET_ALL_GAME_METRIC_BASE_URL = 'http://localhost:8083/games/view-all-metrics'
+export const CREATE_GAME_METRIC_BASE_URL = 'http://localhost:8083/games/create-metric'
+export const UPDATE_GAME_METRIC_BASE_URL = 'http://localhost:8083/games/update-metric'
 
