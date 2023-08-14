54bbfaf227761720e324f740738fd47d64f76850
Soh Thiam Hing <daffodilistic@gmail.com>
2023-08-14 23:22:48 +0800

    [devops] Add notes for build script

--- deploy-appengine.ps1
+++ deploy-appengine.ps1
@@ -1,3 +1,4 @@
 cd rigsg
 .\mvnw.cmd install
-.\mvnw --projects info-collection-system --also-make clean package spring-boot:repackage appengine:deploy -D skipTests=true -D app.deploy.projectId=sapient-symbol-388402 -D app.deploy.version=test-v1
+# NOTE server port specified via CLI takes precedence over application.yml
+.\mvnw.cmd --projects info-collection-system --also-make clean package spring-boot:repackage appengine:deploy -D server.port=8080 -D skipTests=true -D app.deploy.projectId=sapient-symbol-388402 -D app.deploy.version=test-v1
