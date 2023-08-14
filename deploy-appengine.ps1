57ef8c33de9810ca7a7cdefd6c289b992074c589
Soh Thiam Hing <daffodilistic@gmail.com>
2023-08-14 22:58:23 +0800

    [devops] working build and deploy script for App Engine. Ignore entity-model module for deployment as well.

--- deploy-appengine.ps1
+++ deploy-appengine.ps1
@@ -1,3 +1,3 @@
 cd rigsg
 .\mvnw.cmd install
-.\mvnw  --projects demo clean package spring-boot:repackage appengine:deploy -D app.deploy.projectId=sapient-symbol-388402 -D app.deploy.version=test-v1
+.\mvnw --projects info-collection-system --also-make clean package spring-boot:repackage appengine:deploy -D skipTests=true -D app.deploy.projectId=sapient-symbol-388402 -D app.deploy.version=test-v1
