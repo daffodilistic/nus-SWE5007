3c30b9f341915fa80ddd578c99d5370de874339e
PngKianMing <km.png.1988@gmail.com>
2023-08-18 21:05:58 +0800

    updated delete api path

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,6 +2,7 @@
 export const GET_ALL_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/view-all-users'
 export const UPDATE_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/update-user'
 export const CREATE_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/create-user'
+export const DELETE_USER_INFO_BASE_URL ='http://localhost:8081/userinfo/delete-user'
 
 //IDC TEAM
 export const GET_ALL_IDC_TEAM_BASE_URL ='http://localhost:8082/idcteam/view-all-teams'
@@ -10,6 +11,7 @@ export const UPDATE_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/update-te
 export const ADD_MEMBER_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/assign-user'
 export const CREATE_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/create-team'
 export const QUALIFY_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/qualify-team'
+export const DELETE_IDC_TEAM_BASE_URL = 'http://localhost:8082/idcteam/delete-team'
 
 //GAME TEAM
 export const GET_ALL_GAME_TEAM_BASE_URL = 'http://localhost:8083/games/view-all-teams'
@@ -24,6 +26,7 @@ export const GET_ALL_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/vie
 export const CREATE_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/create-metric'
 export const UPDATE_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/update-metric'
 export const CALCULATE_IDC_SCORE_BASE_URL = 'http://localhost:8082/idcmetrics/calculate'
+export const DELETE_IDC_METRIC_BASE_URL = 'http://localhost:8082/idcmetrics/delete-metrics'
 
 //GAME METRICS
 export const GET_ALL_GAME_METRIC_BASE_URL = 'http://localhost:8083/games/view-all-metrics'
@@ -35,13 +38,17 @@ export const GET_ALL_IDC_GROUP_BASE_URL = 'http://localhost:8082/idcgroup/view-a
 export const VIEW_IDC_GROUP_BASE_URL = 'http://localhost:8082/idcgroup/view-group'
 export const CREATE_IDC_GROUP_BASE_URL = 'http://localhost:8082/idcgroup/create-group'
 export const ADD_TEAM_IDC_GROUP_BASE_URL = 'http://localhost:8082/idcgroup/assign-team'
-export const UPDATE_IDC_GROUP_BASE_URL = 'http://localhost:8082/idcgroup/assign-team'
+export const UPDATE_IDC_GROUP_BASE_URL = 'http://localhost:8082/idcgroup/update-group'
+export const DELETE_IDC_GROUP_BASE_URL = 'http://localhost:8082/idcgroup/delete-group'
 
 //GAME Group
 export const GET_ALL_GAME_GROUP_BASE_URL = 'http://localhost:8082/gamegroup/view-all-groups'
 export const VIEW_GAME_GROUP_BASE_URL = 'http://localhost:8082/gamegroup/view-group'
 export const CREATE_GAME_GROUP_BASE_URL = 'http://localhost:8082/gamegroup/create-group'
 export const ADD_TEAM_GAME_GROUP_BASE_URL = 'http://localhost:8082/gamegroup/assign-team'
-export const UPDATE_GAME_GROUP_BASE_URL = 'http://localhost:8082/idcgroup/assign-team'
+export const UPDATE_GAME_GROUP_BASE_URL = 'http://localhost:8082/gamegroup/update-group'
 
+
+//GAMES
+export const GET_ALL_GAMES_BASE_URL = 'http://localhost:8083/games/games'
 
