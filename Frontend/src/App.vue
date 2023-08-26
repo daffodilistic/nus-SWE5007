b59beb44604a59fba3081b9963d846d3654e367b
PngKianMing <km.png.1988@gmail.com>
2023-08-26 21:14:12 +0800

    menu differentiation by user role

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -3,13 +3,15 @@
     <nav class="top-menu">
       <router-link to="/" class="menu-item">Home</router-link>
       <router-link to="/registration" class="menu-item">Register Team</router-link>
-      <router-link v-if="authenticated" to="/upload" class="menu-item">Upload</router-link>
-      <router-link to="/manageContestant" class="menu-item">Manage Contestant</router-link>
-      <router-link to="/manageGroup" class="menu-item">Manage Group</router-link>
-      <router-link to="/score" class="menu-item">Score</router-link>
-      <router-link to="/manageTeam" class="menu-item">Manage Team</router-link>
-      <router-link to="/manageMetric" class="menu-item">Manage Metric</router-link>
+      <router-link v-if="authenticated && userRoles.includes('participant')" to="/contestantUpload" class="menu-item">Upload</router-link>
+      <router-link v-if="authenticated && userRoles.includes('participant')" to="/contestantProfile" class="menu-item">Team Profile</router-link>
+      <router-link v-if="authenticated && userRoles.includes('admin')" to="/manageContestant" class="menu-item">Manage Contestant</router-link>
+      <router-link v-if="authenticated && userRoles.includes('admin')" to="/manageGroup" class="menu-item">Manage Group</router-link>
+      <router-link v-if="authenticated && userRoles.includes('judge')" to="/score" class="menu-item">Score</router-link>
+      <router-link v-if="authenticated && userRoles.includes('admin')" to="/manageTeam" class="menu-item">Manage Team</router-link>
+      <router-link v-if="authenticated && userRoles.includes('admin')" to="/manageMetric" class="menu-item">Manage Metric</router-link>
       <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace class="menu-item">Logout</router-link>
+
       <router-link v-else to="/login" v-on:click.native="login()" class="menu-item">Login</router-link>
     </nav>
     <router-view @authenticated="setAuthenticated" />
@@ -17,34 +19,43 @@
 </template>
 
 <script>
-import Vue from 'vue'
-    export default {
-        name: 'App',
-        data() {
-            return {
-                authenticated: Vue.$keycloak.token.length > 0,
-                // this is only for testing purposes no actual app will have this
-                // an api call will validate user credentials
-                mockAccount: {
-                    username: "km",
-                    password: "km"
-                }
-            }
-        },
-        methods: {
-            setAuthenticated(status) {
-                this.authenticated = status;
-            },
-            logout() {
-                this.authenticated = false;
-                Vue.$keycloak.logout()
-            },
-            login() {
-                this.authenticated = true;
-                Vue.$keycloak.login()
-            }
-        }
+import Vue from 'vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      authenticated: false,
+      userRoles: []
+    };
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
+      if (this.authenticated) {
+        this.checkAuthentication();
+      }
+    },
+    checkAuthentication() {
+      if (this.authenticated) {
+        const tokenData = Vue.$keycloak.tokenParsed;
+        this.userRoles = tokenData.realm_access.roles;
+        console.log("User roles:", this.userRoles);
+      }
+    },
+    logout() {
+      this.authenticated = false;
+      Vue.$keycloak.logout();
+    },
+    login() {
+      this.authenticated = true;
+      Vue.$keycloak.login();
     }
+  },
+  created() {
+    this.checkAuthentication();
+  }
+};
 </script>
 
 <style scoped>
