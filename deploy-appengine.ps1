7b37228c162bf24c76c1eb18fff882ccd2c74d13
Soh Thiam Hing <daffodilistic@gmail.com>
2023-09-11 17:45:15 +0800

    [devops] add CLI arguments to be used for testing deployment

--- deploy-appengine.ps1
+++ deploy-appengine.ps1
@@ -1,8 +1,17 @@
+param (
+    [string]$applicationYml = ""
+)
 $env:APP_ENGINE_PROJECT = "shaped-splicer-397009"
-#$env:APP_ENGINE_SERVICE = "demo"
-$env:APP_ENGINE_SERVICE = "info-collection-system"
+#$env:PROJECT_NAME = "demo"
+$env:PROJECT_NAME = "info-collection-system"
 $env:PORT = 8080
-cd rigsg
+Set-Location rigsg
+if ($applicationYml -ne "") {
+    # Backup existing application.yml
+    Copy-Item -Path .\$env:PROJECT_NAME\src\main\resources\application.yml -Destination .\$env:PROJECT_NAME\src\main\resources\application.yml.bak -Force
+    # Replace application.yml with the specified file
+    Copy-Item -Path .\$env:PROJECT_NAME\src\main\resources\$applicationYml -Destination .\$env:PROJECT_NAME\src\main\resources\application.yml -Force
+}
 .\mvnw.cmd install
 # NOTE server port specified via CLI takes precedence over application.yml
-.\mvnw.cmd --projects $env:APP_ENGINE_SERVICE --also-make clean package spring-boot:repackage appengine:deploy -D server.port=$env:PORT -D skipTests=true -D app.deploy.projectId=$env:APP_ENGINE_PROJECT -D app.deploy.version=test-v1
+.\mvnw.cmd --projects $env:PROJECT_NAME --also-make clean package spring-boot:repackage appengine:deploy -D server.port=$env:PORT -D skipTests=true -D app.deploy.projectId=$env:APP_ENGINE_PROJECT -D app.deploy.version=test-v1
