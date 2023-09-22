4f84b4bc37ddcc7e32d061e3fa3726611d21df3a
Soh Thiam Hing <daffodilistic@gmail.com>
2023-09-22 18:05:39 +0800

    [devops] Initial work on normal Docker image deployment as App Engine is unreliable

--- deploy-docker.ps1
+++ deploy-docker.ps1
@@ -2,12 +2,12 @@ param (
     [string]$applicationYml = ""
 )
 $env:APP_ENGINE_PROJECT = "shaped-splicer-397009"
-#$env:PROJECT_NAME = "demo"
-$env:PROJECT_NAME = "info-collection-system"
+$env:PROJECT_NAME = "demo"
+#$env:PROJECT_NAME = "info-collection-system"
 $env:POSTGRESQL_USERNAME = "prd-ric-app-user"
 $env:POSTGRESQL_PASSWORD = "password"
 $env:PORT = 8080
-Set-Location rigsg
+Push-Location rigsg
 if ($applicationYml -ne "") {
     # Backup existing application.yml
     Copy-Item -Path .\$env:PROJECT_NAME\src\main\resources\application.yml -Destination .\$env:PROJECT_NAME\src\main\resources\application.yml.bak -Force
@@ -16,8 +16,17 @@ if ($applicationYml -ne "") {
 }
 .\mvnw.cmd install
 # NOTE server port specified via CLI takes precedence over application.yml
-.\mvnw.cmd --projects $env:PROJECT_NAME --also-make clean package spring-boot:repackage appengine:deploy `
+.\mvnw.cmd --projects $env:PROJECT_NAME --also-make clean package spring-boot:repackage `
     -D server.port=$env:PORT `
     -D skipTests=true `
     -D app.deploy.projectId=$env:APP_ENGINE_PROJECT `
     -D app.deploy.version=test-v6
+New-Item -Type Directory -Path .\..\docker\volumes\$env:PROJECT_NAME -Force
+Copy-Item -Path .\$env:PROJECT_NAME\target\$env:PROJECT_NAME.jar -Destination .\..\docker\volumes\$env:PROJECT_NAME\$env:PROJECT_NAME.jar -Force
+Pop-Location
+docker run --publish "8080:$env:PORT" `
+    --volume "./docker/volumes/$env:PROJECT_NAME:/application" `
+    --workdir "/application" `
+    eclipse-temurin:17-jre `
+    java `
+    -jar $env:PROJECT_NAME.jar
