71725935e7d69a4b5aca9b546fc79dc180213815
PngKianMing <km.png.1988@gmail.com>
2023-09-30 23:12:34 +0800

    added logics for elimination round

--- Frontend/src/views/ScoreGA.vue
+++ Frontend/src/views/ScoreGA.vue
@@ -58,7 +58,14 @@
       </b-card-header>
       <b-collapse id="first-accordion" accordion="my-accordion" role="tabpanel">
         <div v-if="groups && groups.length > 0">
-        <p>Showing {{ startIndex }} to {{ endIndex }} of {{ totalRecords }} records</p>
+        <table class="instruction-table">
+          <tr><u>Instructions :</u></tr>
+          <tr>1. Click on <b-icon icon="collection-play"></b-icon> to create matchups for all the teams in the group</tr>
+          <tr>2. Click on <i :class="'fas fa-plus'" class="expand-icon"></i> to view team matchups <b-icon icon="arrow-right-circle-fill"></b-icon> Click on <b-icon icon="play-circle"></b-icon> to start the match</tr>
+          <tr>3. After the match is completed, enter score for participating teams <b-icon icon="arrow-right-circle-fill"></b-icon> Click on <b-icon icon="save"></b-icon> to submit the score</tr>
+          <tr>4. After all the matchups are completed, click on  <b-icon icon="star"></b-icon> to qualify the top 2 teams in the group</tr>
+          <tr>5. Repeat step 1 to 4 for all groups</tr>
+        </table>
 
         <table class="main-table">
           <thead>
@@ -84,10 +91,10 @@
               </td>
               <td>
                  <div v-if="group.gameTeamResponses.filter(team => team.isQualifiedForElimination).length >= 2">
-                  Elimination Stage
+                  Completed
                 </div>
                 <div v-else>
-                 Qualification Stage
+                 In-Progress
                 </div>
               </td>
               <td>
@@ -128,7 +135,7 @@
                   </thead>
                   <tbody>
                     <tr v-if="groupedData.length === 0">
-                      <td colspan="11"  style="color: red;">0 game created in the group</td>
+                      <td colspan="11"  style="color: red;">0 game created in the group. Click on <b-icon icon="collection-play"></b-icon> to create games!</td>
                     </tr>
                      <template v-for="(group, groupIndex) in groupedData">
 
@@ -138,7 +145,7 @@
                         <td class="host-td">
                          <span v-if="item.gameOutcome === 'win' && item.gameStatus==='done'"
                           >
-                            <i class="fas fa-trophy gold-trophy"></i>
+                            <i class="fas fa-star gold-star"></i>
                           </span> <br>
                         {{ item.hostTeamName  }}</td>
 
@@ -155,7 +162,7 @@
 
                         <td class="oppo-td"><span v-if="item.gameOutcome === 'lose' && item.gameStatus==='done'"
                           >
-                            <i class="fas fa-trophy gold-trophy"></i><br>
+                            <i class="fas fa-star gold-star"></i><br>
                           </span>{{ item.oppoTeamName  }} </td>
 
                         <td>{{ gameStatusTextMap[item.gameStatus] }}</td>
@@ -203,95 +210,39 @@
           v-b-toggle.second-accordion
           variant="info"
           class="accordion-button"
+          @click="loadGroup"
         >
           Click to view Elimination Round
         </b-button>
       </b-card-header>
       <b-collapse id="second-accordion" accordion="my-accordion" role="tabpanel">
-              <div v-if="groups && groups.length > 0">
-        <p>Showing {{ startIndex }} to {{ endIndex }} of {{ totalRecords }} records</p>
+         <b-button
+      @click="startElimination()"
+      variant="outline-primary"
+      v-b-tooltip.hover="'Click to Kick Start Elimination Round!'"
+      v-if="shouldShowStartButton"
+       style="width: 100%; height: 80px;"
+    >
+            <b-icon icon="play-circle" style="font-size: 36px; line-height: 148px;"></b-icon>&nbsp;Start
+            </b-button>
 
-        <table class="main-table">
-          <thead>
-            <tr>
-              <th>Matchup</th>
-              <th class="longer-td">Team Name</th>
-              <th>Team Score</th>
-              <th></th>
-              <th>Team Score</th>
-              <th class="longer-td">Team Name</th>
-              <th>Game Status</th>
-              <th>Action</th>
-            </tr>
-            </thead>
-           <tbody>
-                    <tr v-if="groupedData.length === 0">
-                      <td colspan="11"  style="color: red;">0 game created in the group</td>
-                    </tr>
-                     <template v-for="(group, groupIndex) in groupedData">
-
-                      <tr v-for="(item, rowIndex) in group" :key="`${groupIndex}`">
-                        <td>{{ groupIndex+1}}</td>
-
-                        <td class="host-td">
-                         <span v-if="item.gameOutcome === 'win' && item.gameStatus==='done'"
-                          >
-                            <i class="fas fa-trophy gold-trophy"></i>
-                          </span> <br>
-                        {{ item.hostTeamName  }}</td>
-
-                        <td v-if="item.gameStatus!=='done'">
-                        <input type="number" v-model="item.enteredHostScore" :min="0" :disabled="item.gameStatus !== 'ongoing'" class="narrow-input">
-                        </td>
-                        <td v-else-if ="item.gameStatus==='done'" class="host-score-td">
-                          {{ item.hostScore }}
-                        </td>
-                        <td>  <img src="../assets/Versus_icon.png" alt="Versus"></td>
-                        <td v-if="item.gameStatus!=='done'">
-                        <input type="number" v-model="item.enteredOppoScore" :min="0" :disabled="item.gameStatus !== 'ongoing'" class="narrow-input"></td>
-                        <td v-else-if ="item.gameStatus==='done'" class="oppo-score-td">{{ item.oppoScore }}</td>
-
-                        <td class="oppo-td"><span v-if="item.gameOutcome === 'lose' && item.gameStatus==='done'"
-                          >
-                            <i class="fas fa-trophy gold-trophy"></i><br>
-                          </span>{{ item.oppoTeamName  }} </td>
-
-                        <td>{{ gameStatusTextMap[item.gameStatus] }}</td>
-                        <td v-if="gameStatusTextMap[item.gameStatus]==='Not Yet Started'">
-                           <b-button @click="startGame(item.id, groupIndex)" variant="outline-primary" class="delete-button" v-b-tooltip.hover="'Click to start game for this matchup'">
-                            <b-icon icon="play-circle"></b-icon>&nbsp;Start
-                            </b-button>
-                        </td>
-
-                        <td
-                          v-if="item.gameStatus == 'ongoing'">
-                           <b-button @click="submitScore(item.hostTeamId,item.oppoTeamId,item, groupIndex,)" variant="outline-primary" class="delete-button" v-b-tooltip.hover="'Click to submit score for this matchup'">
-                            <b-icon icon="save"></b-icon>&nbsp;Score
-                            </b-button>
-                        </td>
-                        <td
-                          v-if="item.gameStatus == 'done'"
-                        >
-                          &nbsp;
-                        </td>
-
-                      </tr>
-                    </template>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <div v-if="!shouldShowStartButton">
+        <div v-if="groups && groups.length > 0">
+          <div class="tournament-bracket">
+            <Round :roundMatches="round1" :isSecondRound="true" />
+            <Round :roundMatches="round2" :isSecondRound="true" />
+            <Round :roundMatches="round3" :isLastRound="true" />
+          </div>
+        </div>
       <div v-else>
           <!-- Show a loading message or spinner while the data is being fetched -->
           <div class="loader-container">
             <i class="fas fa-spinner fa-spin"></i>
           </div>
-        </div>
+        </div></div>
        </b-collapse>
     </b-card>
+
       </div>
 </template>
 
@@ -302,8 +253,12 @@ import { ageGroupOptions, competitionChoiceOptions} from "../dropdownOptions";
 import { VIEW_GAME_GROUP_BASE_URL,GET_ALL_GAME_GROUP_BASE_URL,GET_ALL_GAME_TEAM_BASE_URL,CREATE_GAME_BASE_URL,GET_ALL_GAMES_BASE_URL,UPDATE_GAME_ONGOING_STATUS_BASE_URL,UPDATE_GAME_SCORE_BASE_URL,CHECK_GAME_QUALIFICATION_STATUS_BASE_URL,QUALIFY_GAME_TEAM_BASE_URL} from '@/api';
 import Swal from 'sweetalert2';
 import Vue from 'vue'
+import Round from './Round.vue';
 
 export default {
+  components: {
+    Round,
+  },
   head() {
     return {
       link: [
@@ -320,6 +275,21 @@ export default {
 
   data() {
     return {
+      round1: [
+        { id: 1, homeTeam: 'Team A', awayTeam: 'Team B', homeScore: 1, awayScore: 2 },
+        { id: 2, homeTeam: 'Team C', awayTeam: 'Team D', homeScore: 3, awayScore: 4 },
+        { id: 2, homeTeam: 'Team C', awayTeam: 'Team D', homeScore: 1, awayScore: 2 },
+        { id: 2, homeTeam: 'Team C', awayTeam: 'Team D', homeScore: 1, awayScore: 4 },
+
+
+      ],
+      round2: [
+        { id: 3, homeTeam: 'Winner 1', awayTeam: 'Winner 2', homeScore: 1, awayScore: 2 },
+        { id: 2, homeTeam: 'Team C', awayTeam: 'Team D', homeScore: 3, awayScore: 1 },
+      ],
+      round3: [
+        { id: 4, homeTeam: 'Champion', awayTeam: 'Second Place', homeScore: 1, awayScore: 2 },
+      ],
       currentPageModal: 1,
       itemsPerPageModal: 10,
       currentGroupId: "",
@@ -339,9 +309,16 @@ export default {
       objectsWithIdAndTeamArray : [],
       gameTeamList: [],
       showGAHistoryModal: false,
+      allGameData: [],
     };
   },
   computed: {
+
+     shouldShowStartButton() {
+      // Calculate whether to show the button based on your condition
+      const elim01Count = this.allGameData.filter(item => item.stage === "Elim-01").length;
+      return elim01Count <= 3;
+    },
 
     gameStatusTextMap() {
     // Define a mapping of age group values to their corresponding text
@@ -485,7 +462,6 @@ export default {
           gameScoreOppo : groupObj.enteredOppoScore,
         };
 
-
       const headers = {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${Vue.$keycloak.token}`
@@ -493,7 +469,7 @@ export default {
         console.log(requestBody)
       try {
        const response = await axios.put(`${UPDATE_GAME_SCORE_BASE_URL}`,requestBody, { headers });
-       this.activeRow = this.activeRow === groupIndex ? null : groupIndex;
+      this.toggleRow(groupIndex)
       }
       catch (error) {
         // Handle errors, if any
@@ -529,7 +505,7 @@ export default {
       for (let i = 0; i < group.gameTeamResponses.length; i++) {
 
         if(group.gameTeamResponses[i].qualificationRoundNumMatchesPlayed < 3){
-          counter = true
+          counter = false
         }
       }
 
@@ -552,7 +528,6 @@ export default {
           // If the group has an ID, update the existing record using a PUT request
           const response = await axios.post(`${CHECK_GAME_QUALIFICATION_STATUS_BASE_URL}`, requestBody, { headers });
 
-
           if(response.data.data='Group ',group.id,' is ready for qualification'){
 
             const response = await axios.post(`${QUALIFY_GAME_TEAM_BASE_URL}`, requestBody, { headers });
@@ -562,6 +537,7 @@ export default {
               icon: 'success'
             });
           }
+          this.loadGroup();
         }
         catch (error) {
         // Handle errors, if any
@@ -605,7 +581,11 @@ export default {
         'Authorization': `Bearer ${Vue.$keycloak.token}`
       };
       const teams = group.gameTeamResponses; // Assuming your group object has a 'teams' property containing an array of teams
+      const currentDate = new Date();
+      const isoDateTime = currentDate.toISOString(); // Generates date-time in "YYYY-MM-DDTHH:mm:ss.sssZ" format
 
+      // Now, format it to "YYYY-MM-DDTHH:mm:ssZ" format
+      const formattedDateTime = isoDateTime.slice(0, 19) + "Z";
     // Loop through each team in the group
     for (let i = 0; i < teams.length; i++) {
       for (let j = i + 1; j < teams.length; j++) {
@@ -613,6 +593,8 @@ export default {
         const requestBody = {
           gameTeamIdHost: teams[i].id,
           gameTeamIdOppo: teams[j].id,
+          datetime:formattedDateTime,
+          stage:'Quali'
         };
 
         try {
@@ -632,19 +614,75 @@ export default {
         }
       }
     },
+    async startElimination() {
+          let firstPlaceArray = [];
+          let secondPlaceArray = [];
+          //loop through all game group, to get its 1st and 2nd place teams.
+         for (let index = 0; index < this.groups.length; index++) {
+          const grp =  this.groups[index];
+          for(let index = 0; index < grp.gameTeamResponses.length; index++){
+            if(grp.gameTeamIdFirst === grp.gameTeamResponses[index].id && grp.gameTeamResponses[index].isQualifiedForElimination){
+              const details = {
+                firstTeamID: grp.gameTeamIdFirst,
+                groupId:grp.id,
+                teamName:grp.gameTeamResponses[index].teamName
+              }
+              firstPlaceArray.push(details);
+           }
+           if(grp.gameTeamIdSecond === grp.gameTeamResponses[index].id && grp.gameTeamResponses[index].isQualifiedForElimination){
+            const details = {
+              secondTeamID: grp.gameTeamIdSecond,
+              groupId:grp.id,
+              teamName:grp.gameTeamResponses[index].teamName
+            }
+            secondPlaceArray.push(details);
+           }
+          }
+        }
+         const headers = {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${Vue.$keycloak.token}`
+        };
+        let i = firstPlaceArray.length - 1
+         for (let index = 0; index < firstPlaceArray.length; index++) {
+             try {
+              const requestBody = {
+                  gameTeamIdHost: firstPlaceArray[index].firstTeamID,
+                  gameTeamIdOppo: secondPlaceArray[i].secondTeamID,
+                  stage:'Elim-01'
+              };
+
+             const response = await axios.post(`${CREATE_GAME_BASE_URL}`,requestBody, { headers });
+              Swal.fire({
+              title: 'Success!',
+              text: 'Elimination Round Started!',
+              icon: 'success'
+            });
+            this.loadGroup();
+
+            } catch (error) {
+              console.error("Error fetching data:", error);
+            }
+            i--
+         }
+
+    },
 
     async loadGroup() {
+
       const headers = {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${Vue.$keycloak.token}`
       };
       try {
-
           this.groupsData = await axios.get(`${GET_ALL_GAME_GROUP_BASE_URL}`, { headers });
           this.groups = this.groupsData.data.data;
+          const response = await axios.get(`${GET_ALL_GAMES_BASE_URL}`, { headers });
+          this.allGameData = response.data.data
         } catch (error) {
           console.error("Error fetching data:", error);
         }
+        console.log('loaded group',this.allGameData)
     },
 
     async fetchTeams() {
@@ -698,9 +736,11 @@ export default {
         // Iterate through the data objects
         for (const item of this.groupMembers) {
           // Create an object with "id" and associated teamArray
-          const gameTeamIdHost = this.teamList.find(team => team.id === item.gameTeamIdHost);
 
-          const gameTeamIdOppo = this.teamList.find(team => team.id === item.gameTeamIdOppo);
+          if(item.stage.indexOf('Elim')){
+            const gameTeamIdHost = this.teamList.find(team => team.id === item.gameTeamIdHost);
+
+            const gameTeamIdOppo = this.teamList.find(team => team.id === item.gameTeamIdOppo);
 
           if (tempArray.includes(item.gameTeamIdOppo)) {
             const objectWithIdAndOppoArray = {
@@ -715,7 +755,7 @@ export default {
               oppoTeamName : gameTeamIdOppo.teamName
             };
             this.objectsWithIdAndTeamArray.push(objectWithIdAndOppoArray);
-          }
+          }}
           /*
           if (tempArray.includes(item.gameTeamIdHost)) {
             const objectWithIdAndHostArray = {
@@ -746,6 +786,16 @@ export default {
 
     async viewGAScore(groupObj) {
 
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${Vue.$keycloak.token}`
+      };
+      try {
+          this.groupsData = await axios.get(`${GET_ALL_GAME_GROUP_BASE_URL}`, { headers });
+          this.groups = this.groupsData.data.data;
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
         let groupFilteredObj = this.groups.filter(team => team.id === groupObj.id);
         this.gameTeamList = groupFilteredObj[0].gameTeamResponses
 
@@ -952,7 +1002,7 @@ input.form-control.editing-textbox {
   margin-top: 10px;
   margin-right: 27px;
 }
-.gold-trophy {
+.gold-star {
   color: gold; /* Apply gold color to the trophy icon */
 }
 
@@ -1009,5 +1059,40 @@ input.form-control.editing-textbox {
   background-color: #0056b3;
   color: #ffffff;
 }
+
+.tournament-bracket {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 20px; /* Add margin for spacing */
+  justify-content: space-between; /* Evenly distribute space */
+  width: 77%; /* Adjust as per your layout requirement */
+  margin: 0 auto; /* Center the entire bracket */
+  position: relative;
+}
+
+/* Styling for arrow lines between rounds*/
+.arrow {
+  width: 0;
+  height: 0;
+  border-top: 10px solid transparent;
+  border-bottom: 10px solid transparent;
+  border-left: 15px solid #333; /* Arrow color */
+  right: 655px;
+  position: absolute; /* Position the arrow absolutely within the container */
+  top: 59%; /* Vertically center the arrow within the container */
+  transform: translateY(-50%); /* Adjust for vertical centering */
+}
+
+.instruction-table {
+  text-align: left;
+  font-size: 14px;
+  color: #120b51;
+  margin-left: 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+/* Create connecting lines using pseudo-elements */
 
 </style>
