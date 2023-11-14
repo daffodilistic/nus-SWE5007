082b602f8997c0ee0faab36baca8e700ded33325
Soh Thiam Hing <daffodilistic@gmail.com>
2023-11-14 17:53:48 +0800

    [devops] Update Caddyfile with load balancing config. Make deploy-appengine.ps1 script more flexible by using env vars

--- devops/deploy-appengine.ps1
+++ devops/deploy-appengine.ps1
@@ -1,13 +1,13 @@
 param (
     [string]$applicationYml = ""
 )
-$env:APP_ENGINE_PROJECT = "shaped-splicer-397009"
-#$env:PROJECT_NAME = "demo"
-$env:PROJECT_NAME = "info-collection-system"
+$env:APP_VERSION = "1"
+$env:APP_ENGINE_PROJECT = "apt-terrain-401911"
+$env:PROJECT_NAME = "demo"
 $env:POSTGRESQL_USERNAME = "prd-ric-app-user"
 $env:POSTGRESQL_PASSWORD = "password"
 $env:PORT = 8080
-Set-Location rigsg
+Set-Location ..\rigsg
 if ($applicationYml -ne "") {
     # Backup existing application.yml
     Copy-Item -Path .\$env:PROJECT_NAME\src\main\resources\application.yml -Destination .\$env:PROJECT_NAME\src\main\resources\application.yml.bak -Force
@@ -16,8 +16,8 @@ if ($applicationYml -ne "") {
 }
 .\mvnw.cmd install
 # NOTE server port specified via CLI takes precedence over application.yml
-.\mvnw.cmd --projects $env:PROJECT_NAME --also-make clean package spring-boot:repackage appengine:deploy `
+.\mvnw.cmd -e -X --projects $env:PROJECT_NAME --also-make clean package spring-boot:repackage appengine:deploy `
     -D server.port=$env:PORT `
     -D skipTests=true `
     -D app.deploy.projectId=$env:APP_ENGINE_PROJECT `
-    -D app.deploy.version=test-v6
+    -D app.deploy.version=$env:APP_VERSION `
